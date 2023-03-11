093d0f59ea992f24a0e4c1059079cbfdc778e890
giogorelashvili <gorelashvili0@gmail.com>
2023-03-11 16:24:03 +0400

    03/11/2023
    Last update

--- assets/javascript/Main.js
+++ assets/javascript/Main.js
@@ -1,5 +1,19 @@
-document.getElementById("index").onclick = function() {myFunction()};
+const headerNav = document.querySelector(".header-nav");
+const navList = document.querySelectorAll(".nav-list");
+const burger= document.querySelector(".burger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
 
-function myFunction() {
-  document.getElementById("burger").classList.toggle("show");
+function toggleMenu() {
+  if (headerNav.classList.contains("showMenu")) {
+    headerNav.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    headerNav.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    navList.style.display = "none";
+  }
 }
+
+burger.addEventListener("click", toggleMenu);
