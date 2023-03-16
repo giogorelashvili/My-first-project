2a0a89592347b9dfe803f6a58a2d56000ebce6ba
giogorelashvili <gorelashvili0@gmail.com>
2023-03-16 22:45:18 +0400

    3/16/23
    3/16/23 Last update with JS

--- assets/javascript/Main.js
+++ assets/javascript/Main.js
@@ -1,19 +1,76 @@
-const headerNav = document.querySelector(".header-nav");
-const navList = document.querySelectorAll(".nav-list");
-const burger= document.querySelector(".burger");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+alert ("საიტი სატესტო რეჟიმშია!");
 
-function toggleMenu() {
-  if (headerNav.classList.contains("showMenu")) {
-    headerNav.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    headerNav.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    navList.style.display = "none";
-  }
-}
+// console.log ($("#burger"));
 
-burger.addEventListener("click", toggleMenu);
+// document.getElementById ("burger");
+// console.log (document.getElementById ("burger"));
+// $(document).ready(function() {
+//     console.log();
+//     $('#burger').on('click', function() {
+//         // console.log ("clicked");
+//         // $("#headerNav").addClass("active");
+//         if($("#headerNav").hasClass("active")){
+//             $("#headerNav").removeClass("active");
+//         }else {
+//             $("#headerNav").addClass("active");
+//         }
+//     })
+// });
+
+$(document).ready(function() {
+    console.log();
+    $('#burger').on('click', function() {
+        $("#headerNav").toggleClass("active");
+    });
+});
+
+$(document).ready(function() {
+    console.log();
+    $('#seeMore').on('click', function() {
+    window.open("https://www.behance.net/gallery/163002689/Zaza-Animated-short-film")
+    });
+});
+
+$(document).ready(function() {
+    console.log();
+    $('#facebook').on('click', function() {
+    window.open("https://www.facebook.com/GanimationSlim")
+    });
+});
+
+$(document).ready(function() {
+    console.log();
+    $('#instagram').on('click', function() {
+    window.open("https://www.instagram.com/giogorelashvili/")
+    });
+});
+
+$(document).ready(function() {
+    console.log();
+    $('#youtube').on('click', function() {
+    window.open("https://www.youtube.com/channel/UCSWQme6-b5WPCFBKN-akuvg")
+    });
+});
+
+$(document).ready(function() {
+    console.log();
+    $('#aboutProject').on('click', function() {
+    window.open("https://www.behance.net/gallery/163002689/Zaza-Animated-short-film")
+    });
+});
+
+$(document).ready(function() {
+    console.log();
+    $('#shortFilmZaza').on('click', function() {
+    window.open("https://www.youtube.com/watch?v=dTU12bZoX6A")
+    });
+});
+
+$(document).ready(function() {
+    console.log();
+    $('#annecy').on('click', function() {
+    window.open("https://www.annecyfestival.com/home")
+    });
+});
+
+
